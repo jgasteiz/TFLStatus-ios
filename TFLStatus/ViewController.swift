cde544ef7c140f4e7f3366d75c0119c622f5fa4e
Javi Manzano <javi.manzano.oller@gmail.com>
2015-10-06 19:41:08 +0100

    Initial Commit

---
+++ TFLStatus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TFLStatus
+//
+//  Created by Javi Manzano on 06/10/2015.
+//  Copyright © 2015 Javi Manzano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
