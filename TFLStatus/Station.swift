e120c1d23574fd99efb8f39ed7231af90315d8c0
Javi Manzano <javi.manzano.oller@gmail.com>
2015-10-08 11:09:32 +0100

    Add Station class

--- TFLStatus/Station.swift
+++ TFLStatus/Station.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class Station {
+    
+    var id: String
+    var name: String
+    var lat: Double
+    var lon: Double
+    
+    init(id: String, name: String, lat: Double, lon: Double) {
+        self.id = id
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+    }
+    
+    func getDisplayName() -> String {
+        let stationName = self.name.stringByReplacingOccurrencesOfString(" Underground Station", withString: "")
+        return "\(stationName): \(self.lat), \(self.lon)"
+    }
+    
+}
