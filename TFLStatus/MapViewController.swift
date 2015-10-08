7d8294691a74cec6149cf252e8025699969eb185
Javi Manzano <javi.manzano.oller@gmail.com>
2015-10-08 21:46:46 +0100

    Add a Map view controller

---
+++ TFLStatus/MapViewController.swift
@@ -0,0 +1,96 @@
+//
+//  MapViewController.swift
+//  TFLStatus
+//
+//  Created by Javi Manzano on 08/10/2015.
+//  Copyright © 2015 Javi Manzano. All rights reserved.
+//
+
+import CoreLocation
+import MapKit
+import UIKit
+
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    let regionRadius: CLLocationDistance = 1000
+    
+    let locationManager = CLLocationManager()
+    
+    var userLocated: Bool = false
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        mapView.showsUserLocation = true
+        mapView.delegate = self
+        
+        self.fetchStations()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            self.locationManager.delegate = self
+            self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            self.locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if userLocated == false {
+            let locValue: CLLocationCoordinate2D = manager.location!.coordinate
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(locValue,
+                regionRadius * 2.0, regionRadius * 2.0)
+            mapView.setRegion(coordinateRegion, animated: true)
+            userLocated = true
+        }
+    }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? Station {
+            let identifier = "pin"
+            var view: MKPinAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+                as? MKPinAnnotationView { // 2
+                    dequeuedView.annotation = annotation
+                    view = dequeuedView
+            } else {
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.rightCalloutAccessoryView = UIButton(type: UIButtonType.DetailDisclosure)
+                
+            }
+            return view
+        }
+        return nil
+    }
+    
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        let station: Station = view.annotation as! Station
+        
+        var appDefaults = Dictionary<String, AnyObject>()
+        appDefaults["station_id"] = station.id
+        NSUserDefaults.standardUserDefaults().registerDefaults(appDefaults)
+        NSUserDefaults.standardUserDefaults().synchronize()
+        
+        performSegueWithIdentifier("showStatus", sender: self)
+    }
+    
+    func fetchStations() -> Void {
+
+        let fetchStatusTask = FetchStatusTask()
+        var allStations: [Station] = fetchStatusTask.getStations()
+        
+        allStations = allStations.sort({$0.name < $1.name})
+        
+        // Add them to the map
+        mapView.addAnnotations(allStations)
+        mapView.showAnnotations(allStations, animated: false)
+    }
+}
