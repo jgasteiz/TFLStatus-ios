383ef69db3b28522cc3690e5c218f20522387f75
Javi Manzano <javi.manzano.oller@gmail.com>
2015-10-08 21:45:17 +0100

    New class, Arrival

---
+++ Arrival.swift
@@ -0,0 +1,31 @@
+//
+//  Arrival.swift
+//  TFLStatus
+//
+//  Created by Javi Manzano on 08/10/2015.
+//  Copyright © 2015 Javi Manzano. All rights reserved.
+//
+
+import Foundation
+
+class Arrival: NSObject {
+    
+    var platformName: String
+    var timeToStation: Int
+    var direction: String
+    var currentLocation: String
+    var towards: String
+    
+    init(platformName: String, timeToStation: Int, direction: String, currentLocation: String, towards: String) {
+        self.platformName = platformName
+        self.timeToStation = timeToStation
+        self.direction = direction
+        self.currentLocation = currentLocation
+        self.towards = towards
+    }
+    
+    func getSummary() -> String {
+        return "\(self.towards) - \(self.timeToStation / 60)"
+    }
+    
+}
